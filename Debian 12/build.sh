5088d296e86165acf1e4baeb3c7c428a342d456a
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-08 22:29:34 +0500

    Update build.sh

--- Debian 12/build.sh
+++ Debian 12/build.sh
@@ -1,26 +1,18 @@
 lb config -d bookworm \
-          --debian-installer live \
-          --debian-installer-distribution bookworm \
-          --debian-installer-gui true \
-          --archive-areas "main contrib non-free" \
-          --debootstrap-options "--variant=minbase" \
           --architectures amd64 \
-          --iso-volume "Alice_$(date +%Y%m%d)" \  
+          --binary-images iso \
+          --mode debian \
+          --distribution bookworm \
+          --iso-volume "Alice_$(date +%Y%m%d)" \
           --archive-areas "main contrib non-free non-free-firmware" \
-          --debootstrap-options "--variant=standard --include=sudo,locales,keyboard-configuration" \
-          --iso-publisher "hanedotoaranea - https://github.com/hanedotoaranea" \
-          --iso-application "GitHub:https://github.com/hanedotoaranea/" \
-          --checksums sha256 \
+          --debian-installer true \
+          --debian-installer-gui true \
           --mirror-bootstrap http://deb.debian.org/debian \
           --mirror-binary http://deb.debian.org/debian \
           --mirror-binary-security http://security.debian.org/debian-security \
-          --win32-loader false \
           --apt-recommends true \
           --security true \
-          --zsync true \
-          --updates true \              
-          --bootloader-timeout 5 \   
-          --hdd-label "ALICE_OS" \
-          --color \
-
+          --updates true \
+          --hdd-label "ALICE_OS" 
+          
 sudo lb build
