ff2c20b93cb4cd68db5bf473301b45ccba2dc05e
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-08 22:36:58 +0500

    Update build.sh

--- Debian 12/build.sh
+++ Debian 12/build.sh
@@ -1,18 +1,21 @@
-lb config -d bookworm \
-          --architectures amd64 \
-          --binary-images iso \
-          --mode debian \
-          --distribution bookworm \
-          --iso-volume "Alice_$(date +%Y%m%d)" \
-          --archive-areas "main contrib non-free non-free-firmware" \
-          --debian-installer true \
-          --debian-installer-gui true \
-          --mirror-bootstrap http://deb.debian.org/debian \
-          --mirror-binary http://deb.debian.org/debian \
-          --mirror-binary-security http://security.debian.org/debian-security \
-          --apt-recommends true \
-          --security true \
-          --updates true \
-          --hdd-label "ALICE_OS" 
-          
+lb config \
+  --distribution bookworm \
+  --architectures amd64 \
+  --binary-images iso-hybrid \
+  --mode debian-cd \
+  --iso-volume "Alice_Full_DVD_$(date +%Y%m%d)" \
+  --archive-areas "main contrib non-free non-free-firmware" \
+  --debian-installer true \
+  --debian-installer-gui false \
+  --mirror-bootstrap http://deb.debian.org/debian \
+  --mirror-binary http://deb.debian.org/debian \
+  --mirror-binary-security http://security.debian.org/debian-security \
+  --hdd-label "ALICE_OS" \
+  --bootloader syslinux \
+  --memtest none \
+  --win32-loader false \
+  --cache-packages true \
+  --apt-indices false \
+  --checksums sha256 \
+  
 sudo lb build
